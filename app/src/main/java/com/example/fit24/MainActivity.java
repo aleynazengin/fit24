2b378bafb80e46814e92eda7f81046da517a805f
aleynazengin <64136162+aleynazengin@users.noreply.github.com>
2020-05-18 16:57:47 +0300

    proje eklendi

--- app/src/main/java/com/example/fit24/MainActivity.java
+++ app/src/main/java/com/example/fit24/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splashekrani);
 
-        Log.d("Maın", "Hello");
     }
 }
