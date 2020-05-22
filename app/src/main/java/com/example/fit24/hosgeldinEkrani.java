e1729706863d5e6387d664619db74276af1a9a12
aleynazengin <64136162+aleynazengin@users.noreply.github.com>
2020-05-23 00:36:17 +0300

    duzenledim

--- app/src/main/java/com/example/fit24/hosgeldinEkrani.java
+++ app/src/main/java/com/example/fit24/hosgeldinEkrani.java
@@ -2,17 +2,24 @@ package com.example.fit24;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class hosgeldinEkrani extends Fragment {
+    Button egzersizegit,idealsayfasinagit,diyetegit;
+
 
     public hosgeldinEkrani() {
         // Required empty public constructor
@@ -23,6 +30,34 @@ public class hosgeldinEkrani extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hosgeldin_ekrani, container, false);
+        View view= inflater.inflate(R.layout.fragment_hosgeldin_ekrani, container, false);
+        egzersizegit=view.findViewById(R.id.buttonegzersiz);
+        idealsayfasinagit=view.findViewById(R.id.buttonideal);
+        diyetegit=view.findViewById(R.id.buttondiyet);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final NavController navController = Navigation.findNavController(view);
+        egzersizegit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_hosgeldinEkrani_to_egzersizListe);
+            }
+        });
+        idealsayfasinagit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_hosgeldinEkrani_to_idealkiloEkran);
+            }
+        });
+        diyetegit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_hosgeldinEkrani_to_diyetListe);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fit24/idealkiloEkran.java
+++ app/src/main/java/com/example/fit24/idealkiloEkran.java
@@ -2,17 +2,23 @@ package com.example.fit24;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class idealkiloEkran extends Fragment {
+    Button egzersizegit,idealsayfasinagit,diyetegit;
 
     public idealkiloEkran() {
         // Required empty public constructor
@@ -23,6 +29,34 @@ public class idealkiloEkran extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_idealkilo_ekran, container, false);
+        View view= inflater.inflate(R.layout.fragment_idealkilo_ekran, container, false);
+        egzersizegit=view.findViewById(R.id.buttonegzersiz2);
+        idealsayfasinagit=view.findViewById(R.id.buttonideal2);
+        diyetegit=view.findViewById(R.id.buttondiyet2);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final NavController navController = Navigation.findNavController(view);
+        egzersizegit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_idealkiloEkran_to_egzersizListe);
+            }
+        });
+        idealsayfasinagit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_idealkiloEkran_self);
+            }
+        });
+        diyetegit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_idealkiloEkran_to_diyetListe);
+            }
+        });
     }
 }
